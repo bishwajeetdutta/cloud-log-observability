f4b8fe37881f5a3732dd86047acf44e26c6c99dc
bishwajeetdutta <bishwajeetdutta33@gmail.com>
2026-04-17 18:56:49 +0000

    Manually monitors and restart, used in phase-1

--- auto_restart.sh
+++ auto_restart.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# =========================================================================
+# Note: This script was used in Phase 1 to manually monitor and restart 
+# the Python process using raw Linux commands. 
+# This becomes obsolete as it is now 100% handled natively by 
+# Docker Compose's 'restart: always' self-healing policy. 
+# =========================================================================
+
 # Define the script to check
 SCRIPT_NAME="log_generator.py"
 
@@ -7,12 +14,12 @@ SCRIPT_NAME="log_generator.py"
 # 'grep -v grep' removes the search command itself from the result
 if ps aux | grep "$SCRIPT_NAME" | grep -v "grep" > /dev/null
 then
-    echo "✅ System Status: $SCRIPT_NAME is running."
+    echo " System Status: $SCRIPT_NAME is running."
 else
-    echo "🚨 ALERT: $SCRIPT_NAME is STOPPED. Restarting now..."
+    echo " ALERT: $SCRIPT_NAME is STOPPED. Restarting now..."
     
     # Restart the script in the background
-    nohup python3 /home/ec2-user/cloud_project/log_generator.py &
+    nohup python3 /home/ec2-user/cloud-log-observability/log_generator.py &
     
-    echo "♻️  Recovery Complete. Service restarted."
+    echo " Recovery Complete. Service restarted."
 fi
